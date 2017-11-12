69ddc5938b5efa286cbc434491494d85228f8989
Luke Pafford <lukepafford@gmail.com>
2017-11-12 01:17:33 -0800

    Updated latest-version-parser.py
    Modified argparse formatter to print both default values, and raw text

--- latest-version-parser.py
+++ latest-version-parser.py
@@ -18,7 +18,7 @@ except ImportError:
 
 def main():
 	argparser = argparse.ArgumentParser(description='Returns a url of the latest software version from a url',
-			formatter_class=argparse.RawDescriptionHelpFormatter,	
+			formatter_class=CustomFormatter,	
 			epilog=dedent('''
 			This program will return the url of the newest version of software available from a url.
 			The program will work against software that follows the pattern from the 
@@ -46,6 +46,8 @@ def main():
 				help='part of string before the version number starts')
 	argparser.add_argument('suffix', type=str,
 				help='part of string after the version number ends')
+	argparser.add_argument('pattern', nargs='?', default='\d+\.\d+\.\d+',
+				help='regex matching the version')
 
 	args = argparser.parse_args()
 	try:
@@ -66,16 +68,20 @@ def main():
 	latest_version = '{0}{1}{2}{3}'.format(args.url, args.prefix, sorted_versions[-1], args.suffix)
 	print(latest_version)
 
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+	    pass
+
 class Latest_Version_Parser(HTMLParser):
 	""" Returns a list of strings containing software versions """
-	def __init__(self, prefix, suffix):
+	def __init__(self, prefix, suffix, pattern):
 		HTMLParser.__init__(self)
 		self.versions = list()
 		self.prefix = prefix
 		self.suffix = suffix
+		self.pattern = pattern
 
 	def handle_data(self, data):
-		pattern = '{0}(\d+\.\d+\.\d+){1}'.format(self.prefix, self.suffix)
+		pattern = self.prefix + self.pattern + self.suffix
 		regex = re.compile(pattern)
 		found = re.search(regex, data)
 		if found:
